387e86422d5e4b381b0332475194ff27b61c88a1
Monkey322 <otta12@gmail.com>
2021-11-23 21:09:38 -0300

    Atualizando mais uma vez

--- src/controller/games.js
+++ src/controller/games.js
@@ -1,12 +1,34 @@
 const games = require("../models/games.json")
+const fs = require("fs")
+
+
+
 
 const getAllgames = (req, res) => {
-    console.log(req.url);
-
+    console.log(req.url)
     res.status(200).send(games)
 }
 
+const creategames = (req, res) => {
+    const { id, Nome, Gênero, Ano, Desenvolvedora } = req.body
+    games.push({ id, Nome, Gênero, Ano, Desenvolvedora })
+    fs.writeFile("./src/models/games.json", JSON.stringify(games), 'utf8', function (err) {
+        if (err) {
+            res.status(500).send({ message: err })
+        } else {
+            console.log("Arquivo atualizado com sucesso!")
+            const gamesFound = games.find(games => games.id == id)     
+            res.status(200).send(gamesFound)
+        }
+
+
+    })
+}
+
+
 
 module.exports = { 
+    creategames,
     getAllgames,
+    
  }
--- src/routes/games.js
+++ src/routes/games.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const controller = require("../controller/games")
 
 router.get("/", controller.getAllgames);
-
+router.post("/", controller.creategames);
 
 module.exports = router;
